9e47d4a50367ce5739c0a5467e3e21190c51e8b0
GagarinAP <gagarin.web@gmail.com>
2016-09-18 14:25:24 +0300

    edit icon marker and form

--- js/map.js
+++ js/map.js
@@ -158,9 +158,19 @@ function initMap() {
                         
     // Info Window Content
     var infoWindowContent = [
-        ['<div class="info_content">' +
-        '<h3>London Eye</h3>' +
-        '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
+        ['<div class="col s6"><div class="card">'+
+    '<div class="card-image waves-effect waves-block waves-light">'+
+      '<img class="activator" src="img/office.jpg">'+
+    '</div>'+
+    '<div class="card-content">'+
+      '<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>'+
+      '<p><a href="#">This is a link</a></p>'+
+    '</div>'+
+    '<div class="card-reveal">'+
+      '<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>'+
+      '<p>Here is some more information about this product that is only revealed once clicked on.</p>'+
+    '</div>'+
+  '</div></div>'],
         ['<div class="info_content">' +
         '<h3>Palace of Westminster</h3>' +
         '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
@@ -173,15 +183,15 @@ function initMap() {
         '</div>'],['<div class="info_content">' +
         '<h3>London Eye</h3>' +
         '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
-        ['<div class="info_content">' +
+        ['<div class="content">' +
         '<h3>Palace of Westminster</h3>' +
         '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-        '</div>']
+        '<img class="responsive-img" src="img/office.jpg"></div>']
     ];
         
     // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
-    
+    var infoWindow = new google.maps.InfoWindow({maxWidth: 400}), marker, i;
+    var markerImage = 'img/marker.png';
     // Loop through our array of markers & place each one on the map  
     for( i = 0; i < markers.length; i++ ) {
         var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
@@ -189,7 +199,8 @@ function initMap() {
         marker = new google.maps.Marker({
             position: position,
             map: map,
-            title: markers[i][0]
+            title: markers[i][0],
+            icon: markerImage
         });
         
         // Allow each marker to have an info window    
@@ -211,3 +222,51 @@ function initMap() {
     });
     
 }
+
+/*
+<!DOCTYPE html>
+<html> 
+<head> 
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+  <title>Google Maps Multiple Markers</title> 
+  <script src="http://maps.google.com/maps/api/js?sensor=false" 
+          type="text/javascript"></script>
+</head> 
+<body>
+  <div id="map" style="width: 500px; height: 400px;"></div>
+
+  <script type="text/javascript">
+    var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  </script>
+</body>
+</html>*/
