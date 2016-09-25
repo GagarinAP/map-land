67bd26fcb3dab8eae2c7260e4b441519dd5c6d25
GagarinAP <gagarin.web@gmail.com>
2016-09-25 11:47:16 +0300

    e v4

--- js/map-new.js
+++ js/map-new.js
@@ -1,4 +1,4 @@
-
+function initMap() {
     var style = [
       {
         featureType: "road.local",
@@ -20,7 +20,7 @@
         ['<div class="container"><div class="row"><div class="col l12 s12"><h3>Лот №6</h3><p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p><img class="col l12 s12 responsive-img" src="img/444.jpg"></div></div></div>', 50.6339021, 26.2668659,6,'img/marker.png']
     ];
     var mapOptions = {
-        mapTypeId: 'roadmap',
+        //mapTypeId: 'roadmap',
         styles: style
     };
 
@@ -28,13 +28,13 @@
       zoom: 12,
       center: new google.maps.LatLng(50.6, 26.2),      
     });
-    map.setTilt(45);
+    //map.setTilt(45);
 
     var infowindow = new google.maps.InfoWindow({maxWidth: 400});
     var bounds = new google.maps.LatLngBounds();
     var marker, i;
     //var markerImage = 'img/marker.png';
-    for (i = 0; i < locations.length; i++) {
+    for (i = 0; i < locations.length; i++) {      
       var position = new google.maps.LatLng(locations[i][1], locations[i][2]);
       bounds.extend(position);  
       marker = new google.maps.Marker({
@@ -56,3 +56,4 @@
         this.setZoom(12);
         google.maps.event.removeListener(boundsListener);
     });
+}
