7de35d511bac09e2c2f5499f2eaed7ac9b632207
GagarinAP <gagarin.web@gmail.com>
2016-09-25 11:31:05 +0300

    e v3

--- js/map-new.js
+++ js/map-new.js
@@ -1,3 +1,4 @@
+
     var style = [
       {
         featureType: "road.local",
@@ -22,6 +23,7 @@
         mapTypeId: 'roadmap',
         styles: style
     };
+
     var map = new google.maps.Map(document.getElementById('map'), mapOptions, {
       zoom: 12,
       center: new google.maps.LatLng(50.6, 26.2),      
